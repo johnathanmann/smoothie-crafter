84becd25233a49b984a245e0d54dd37ebe5cb803
Johnny <johnathan.mnn@gmail.com>
2023-05-11 23:04:38 -0500

    Circles are styled on homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -10,6 +10,7 @@ export default function Homepage(){
 
     function handleProduce(e) {
       localStorage.setItem("produceNumber", e );
+      document.getElementById(e).style.backgroundColor="red";
     }
     function handleSpecial(e) {
       localStorage.setItem("specialNumber", e );
@@ -18,19 +19,16 @@ export default function Homepage(){
     const handleSubmit = (e) => {
         navigate('/generator')
       }
-      $('.button').click(function() {
-        $('.temp-wrapper').append('<div class="temp"><div class="icon-del"></div><div>new content</div></div>');
-      });
     return(
       <div className="container center">
           <div>
-            <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" margin="10px"><button onClick={() => handleProduce(11)}>2</button></RoughNotation>
-            <RoughNotation strokeWidth="2" type="circle" color="#000" animate="true" show="true" ><button onClick={() => handleProduce(10)}>3</button></RoughNotation>
-            <RoughNotation strokeWidth="2" type="circle" color="#000" animate="true" show ><button onClick={() => handleProduce(9)}>4</button></RoughNotation>
+           <button onClick={() => handleProduce(11)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="11" >2</span></RoughNotation></button>
+           <button onClick={() => handleProduce(10)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10"><span className="number-btn" id="10">3</span></RoughNotation></button>
+           <button onClick={() => handleProduce(9)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10"><span className="number-btn" id="9">4</span></RoughNotation></button>
             <br />
-            <RoughNotation strokeWidth="2" type="circle" color="#000" animate="true" show ><button onClick={() => handleSpecial(7)}>2</button></RoughNotation>
-            <RoughNotation strokeWidth="2" type="circle" color="#000" animate="true" show ><button onClick={() => handleSpecial(8)}>3</button></RoughNotation>
-            <RoughNotation strokeWidth="2" type="circle" color="#000" animate="true" show ><button onClick={() => handleSpecial(9)}>4</button></RoughNotation>
+            <button onClick={() => handleSpecial(7)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="7" >2</span></RoughNotation></button>
+            <button onClick={() => handleSpecial(8)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="8" >3</span></RoughNotation></button>
+            <button onClick={() => handleSpecial(9)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="9" >4</span></RoughNotation></button>
             <br/>
             <button onClick={handleSubmit}>Go</button>
           </div>
