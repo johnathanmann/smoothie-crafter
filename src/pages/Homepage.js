29936435a4d55487a3d931fe463310242798c561
Johnny <johnathan.mnn@gmail.com>
2023-05-12 22:17:11 -0500

    Special ingredients rendering in correct amounts

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,17 +8,17 @@ import "../styles/homepage.css";
 export default function Homepage(){
     const navigate = useNavigate();
 
-    function handleProduce(e, c1, c2) {
+    function handleProduce(e, b, c1, c2) {
       localStorage.setItem("produceNumber", e );
       document.getElementById(c1).classList.remove("active");
       document.getElementById(c2).classList.remove("active");
-      document.getElementById(e).classList.add("active");
+      document.getElementById(b).classList.add("active");
     }
-    function handleSpecial(e, c1, c2) {
+    function handleSpecial(e, b, c1, c2) {
       localStorage.setItem("specialNumber", e );
       document.getElementById(c1).classList.remove("active");
       document.getElementById(c2).classList.remove("active");
-      document.getElementById(e).classList.add("active");
+      document.getElementById(b).classList.add("active");
     }
 
     const handleSubmit = (e) => {
@@ -27,13 +27,13 @@ export default function Homepage(){
     return(
       <div className="container center">
           <main>
-           <button onClick={() => handleProduce(11, 10, 9)}> <RoughNotation strokeWidth="2"  type="circle" animate="true"show="true" padding="10" ><span className="number-btn " id="11" >2</span></RoughNotation></button>
-           <button onClick={() => handleProduce(10, 11, 9)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10"><span className="number-btn " id="10">3</span></RoughNotation></button>
-           <button onClick={() => handleProduce(9, 10, 11)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10"><span className="number-btn " id="9">4</span></RoughNotation></button>
+           <button onClick={() => handleProduce(11, "b1", "b2", "b3")}> <RoughNotation strokeWidth="2"  type="circle" animate="true"show="true" padding="10" ><span className="number-btn " id="b1" >2</span></RoughNotation></button>
+           <button onClick={() => handleProduce(10, "b2", "b1", "b3")}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10"><span className="number-btn " id="b2">3</span></RoughNotation></button>
+           <button onClick={() => handleProduce(9, "b3", "b1", "b2")}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10"><span className="number-btn " id="b3">4</span></RoughNotation></button>
             <br />
-            <button onClick={() => handleSpecial(7, 8, 0)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="7" >2</span></RoughNotation></button>
-            <button onClick={() => handleSpecial(8, 7, 0)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="8" >3</span></RoughNotation></button>
-            <button onClick={() => handleSpecial(0, 8, 7)}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="0" >4</span></RoughNotation></button>
+            <button onClick={() => handleSpecial(7, "b4", "b5", "b6")}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="b4" >2</span></RoughNotation></button>
+            <button onClick={() => handleSpecial(6, "b5", "b4", "b6")}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="b5" >3</span></RoughNotation></button>
+            <button onClick={() => handleSpecial(5, "b6", "b4","b5" )}> <RoughNotation strokeWidth="2"  type="circle" color="#000" animate="true"show="true" padding="10" ><span className="number-btn" id="b6" >4</span></RoughNotation></button>
             <br/>
             <button onClick={handleSubmit}>Go</button>
           </main>
